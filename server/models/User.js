7439d3c1fe9c6563f90508bc632f05519cfa5f4e
James <jamesmasher13@gmail.com>
2019-07-04 01:27:02 -0700

    Add user model

---
+++ server/models/User.js
@@ -0,0 +1,21 @@
+import uuid from 'uuid';
+
+import { users } from '../data/store';
+
+export default class User {
+    constructor(email, firstName, lastName, password, phoneNumber, address, isAdmin = false) {
+        this.id = uuid.v4();
+        this.email = email;
+        this.first_name = firstName;
+        this.last_name = lastName;
+        this.password = password;
+        this.phoneNumber = phoneNumber;
+        this.address = address;
+        this.isAdmin = isAdmin;
+    }
+
+    static getByEmail(email) {
+        const res = users.find(user => user.email === email);
+        return res;
+    }
+}
